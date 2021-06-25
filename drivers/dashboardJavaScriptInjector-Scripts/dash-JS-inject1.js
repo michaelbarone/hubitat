b189784be29e4f467185db79f851694b66b65f3c
michaelbarone <mbarone000@gmail.com>
2021-06-25 16:10:58 -0700

    added check so JavaScript does not fully run if embedded on a device page..  this happens when saving the device, it will inject the javascript, but we dont want to have the script execute if its not on a dashboard.

--- drivers/dashboardJavaScriptInjector-Scripts/dash-JS-inject1.js
+++ drivers/dashboardJavaScriptInjector-Scripts/dash-JS-inject1.js
@@ -1,7 +1,13 @@
 (function() {
     function pageStart(){
         //console.log("pageStart called");
-        
+
+        if(window.location.pathname.includes("device/edit")){
+            // log to console that the script was injected properly, but stopped becuase we are on the device page
+            console.log("JavaScript has been injected!  The script was stopped because it was injected into the device/edit page.  This should work on a dashboard.")
+            // stop execution if we are on the device edit page, and not a dashboard.
+            return;
+        }        
         
         
         /* remove specific words from tile titles */
