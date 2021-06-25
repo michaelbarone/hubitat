22b49d5374532e3e8ecb608107324ccfaf04f184
michaelbarone <mbarone000@gmail.com>
2021-06-25 15:35:31 -0700

    updated

--- drivers/iFrameAdvanced.groovy
+++ drivers/iFrameAdvanced.groovy
@@ -41,8 +41,10 @@
 
 preferences {
         input("src", "text", title: "iFrame Url",  required: true)
+        input("basicOrAdvanced", "bool", title: "Off = basic  ---  On = Advanced", defaultValue:false,  required: true)
         input("openText", "text", title: "Button text to Open iFrame", defaultValue:"Show",  required: false)
         input("closeText", "text", title: "Button text to close iFrame", defaultValue:"Close", required: false)
+        input("refreshText", "text", title: "Button text to refresh iFrame content", defaultValue:"Refresh", required: false)
     }
 metadata {
     definition (name: "iFrameAdvanced", namespace: "mbarone", author: "mbarone", importUrl: "https://raw.githubusercontent.com/michaelbarone/hubitat/master/drivers/iFrameAdvanced.groovy") {
@@ -58,6 +60,30 @@ def updated() {
 }
 def setIframe() {
     if(src && src != ""){
-        sendEvent(name: "iFrame", value: "<button onclick=document.getElementById('${device.displayName.replaceAll('\\s','')}').style.display='block';>${openText}</button><div id=${device.displayName.replaceAll('\\s','')} class='modal' style='display: none;position: fixed;top: 0;left: 0;width: 100%;height: 100%;z-index:100;background-color: rgba(0,0,0,.85);'><button onclick=document.getElementById('${device.displayName.replaceAll('\\s','')}-iframe').src=${src}; style='position:fixed;top:5px;right:35px;'>Refresh</button><button onclick=document.getElementById('${device.displayName.replaceAll('\\s','')}').style.display='none'; style='position:fixed;top:5px;right:5px;'>${closeText}</button><iframe id='${device.displayName.replaceAll('\\s','')}-iframe' src=${src} style='height: 100%; width:100%; border: none;'></iframe></div>")
+        if(!basicOrAdvanced){
+            sendEvent(name: "iFrame", value: "<div style='height: 100%; width: 100%'><iframe src='${src}' style='height: 100%; width:100%; border: none;'></iframe><div>")
+        } else {
+            //var iFrameValue = "<div style='height: 100%; width: 100%'><iframe onclick=document.getElementById('${device.displayName.replaceAll('\\s','')}').style.display='block';document.getElementById('${device.displayName.replaceAll('\\s','')}-iframe').src='${src}'; src='${src}' style='height: 100%; width:100%; border: none;'></iframe><div>"
+             //                   +"<div id=${device.displayName.replaceAll('\\s','')} class='modal' style='display:none;position:fixed;top:0;left:0;width:100%;height:100%;z-index:100;background-color:rgba(0,0,0,.85);'>"
+             //                       +"<iframe onclick=document.getElementById('${device.displayName.replaceAll('\\s','')}').style.display='none'; id='${device.displayName.replaceAll('\\s','')}-iframe' src=${src} style='height:100%;width:100%;border:none;'></iframe>"
+             //                   +"</div>"
+
+            //sendEvent(name: "iFrame", value: "<div onclick=document.getElementById('${device.displayName.replaceAll('\\s','')}').style.display='block';document.getElementById('${device.displayName.replaceAll('\\s','')}-iframe').src='${src}'; style='height: 100%; width: 100%'><iframe src='${src}' style='height: 100%; width:100%; border: none;'></iframe><div><div id=${device.displayName.replaceAll('\\s','')} class='modal' style='display:none;position:fixed;top:0;left:0;width:100%;height:100%;z-index:100;background-color:rgba(0,0,0,.85);'><iframe onclick=document.getElementById('${device.displayName.replaceAll('\\s','')}').style.display='none'; id='${device.displayName.replaceAll('\\s','')}-iframe' src=${src} style='height:100%;width:100%;border:none;'></iframe></div>")
+
+            /*
+            var iFrameValue = "<button onclick=document.getElementById('${device.displayName.replaceAll('\\s','')}').style.display='block';>${openText}</button>"
+                                +"<div id=${device.displayName.replaceAll('\\s','')} class='modal' style='display:none;position:fixed;top:0;left:0;width:100%;height:100%;z-index:100;background-color:rgba(0,0,0,.85);'>"
+                                    +"<button onclick=document.getElementById('${device.displayName.replaceAll('\\s','')}').style.display='none'; style='float:right;margin:5px;'>${closeText}</button>"
+                                    +"<button onclick=document.getElementById('${device.displayName.replaceAll('\\s','')}-iframe').src='${src}'; style='float:right;margin:5px;'>Refresh</button>"
+                                    +"<iframe id='${device.displayName.replaceAll('\\s','')}-iframe' src=${src} style='height:100%;width:100%;border:none;'></iframe>"
+                                +"</div>"
+            log.warn iFrameValue
+            sendEvent(name: "iFrame", value: "${iFrameValue}")
+            */
+            // buttons
+            sendEvent(name: "iFrame", value: "<button onclick=document.getElementById('${device.displayName.replaceAll('\\s','')}').style.display='block';>${openText}</button><div id=${device.displayName.replaceAll('\\s','')} class='modal' style='display:none;position:fixed;top:0;left:0;width:100%;height:100%;z-index:100;background-color:rgba(0,0,0,.85);'><button onclick=document.getElementById('${device.displayName.replaceAll('\\s','')}').style.display='none'; style='float:right;margin:5px;'>${closeText}</button><button onclick=document.getElementById('${device.displayName.replaceAll('\\s','')}-iframe').src='${src}'; style='float:right;margin:5px;'>${refreshText}</button><iframe id='${device.displayName.replaceAll('\\s','')}-iframe' src=${src} style='height:100%;width:100%;border:none;'></iframe></div>")
+        }
+    } else {
+        log.warn "No website to embed.  Set iFrame Url in device preferences."
     }
 }
